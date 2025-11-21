bc320c7f9cc29a829fad26bf2883527c57580dd1
Jaiki Sharma <jack912062sharmas@gmail.com>
2025-11-21 19:30:34 +0530

    CSS changing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let users = [];
 
 const app = express();
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -13,12 +14,9 @@ app.get("/", (req, res) => {
 
 app.post("/submit", (req, res) => {
      const { name, email, password} = req.body;
-
-  
     if (!name || !email || !password) {
         return res.send("❌ All fields are required! <br> <a href='/'>Go back</a>");
     }
-
     const user = {name, email, password };
     users.push(user);
 
